40a47b03c9ec09549a60ce3a0969bc1ca11871cb
isubfox <samuel.liska@gmail.com>
2026-07-19 15:57:28 +0200

    feat: display user posts on profile page

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
-import { User } from '../blog-post.interface';
+import { BlogPost, User } from '../blog-post.interface';
 import { BlogService } from '../blog.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css',
 })
@@ -12,8 +13,10 @@ export class UserComponent implements OnInit {
   @Input() id: number = 0;
   private blogService = inject(BlogService);
   user!: User;
+  userPosts!: BlogPost[];
   ngOnInit() {
     const userId = Number(this.id);
     this.user = this.blogService.getUserById(userId);
+    this.userPosts = this.blogService.getPostsByUserId(userId);
   }
 }
