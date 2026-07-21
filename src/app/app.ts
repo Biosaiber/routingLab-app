626fd5b46f1ad906a26df1e3c7c7817d6f29069c
isubfox <samuel.liska@gmail.com>
2026-07-21 19:44:06 +0200

    feat: add programmatic navigation to random post

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component, signal, inject } from '@angular/core';
-import { RouterOutlet, RouterLink } from '@angular/router';
+import { Router, RouterOutlet, RouterLink } from '@angular/router';
 import { Location } from '@angular/common';
+import { BlogService } from './blog.service';
+import { BlogPost } from './blog-post.interface';
 
 
 @Component({
@@ -13,10 +15,20 @@ import { Location } from '@angular/common';
 export class App {
   protected readonly title = signal('routingLab-app');
   private location = inject(Location);
+  private blogService = inject(BlogService);
+  private router = inject(Router);
   goBack() {
     this.location.back();
   }
   goForward() {
     this.location.forward();
   }
+  goRandomPost() {
+    const posts = this.blogService.getPosts();
+    const randomIndex = Math.floor(Math.random() * posts.length);
+    const randomPost = posts[randomIndex];
+
+    this.router.navigate(['/post', randomPost.id]);
+  }
+
 }
